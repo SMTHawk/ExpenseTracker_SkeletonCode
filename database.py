2cda2ec7788a6aa253d90716cfbe0993185d5209
Isanomal <ryanvandommelen@gmail.com>
2024-04-28 19:20:20 -0500

    Fixed the Login issue using DB Browser

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ import sqlite3
 
 class Database:
     def __init__(self, db_name):
-        # Initialize connection
+        #Initialize connection
         self.db_name = db_name
         self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
         print("Database connection established.")
 
     def execute_query(self, query, params=None):
-        # Execute query with optional parameters
+        #Execute query
         try:
             self.cursor.execute(query, params if params else ())
             self.connection.commit()
@@ -18,7 +18,7 @@ class Database:
             print(f"Error executing query: {e}")
 
     def fetch_data(self, query, params=None):
-        # Fetch data with optional parameters
+        #Fetch data
         try:
             self.cursor.execute(query, params if params else ())
             data = self.cursor.fetchall()
@@ -29,7 +29,7 @@ class Database:
             return None
 
     def close_connection(self):
-        # Close connection
+        #Close connection
         self.cursor.close()
         self.connection.close()
         print("Database connection closed.")
