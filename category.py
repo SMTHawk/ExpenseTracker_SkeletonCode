55b25f165233853801bfcd67c8c38feb8e4293d2
Nwhited <159360127+Nwhited@users.noreply.github.com>
2024-05-01 19:54:28 -0500

    If someone can delete this file then do it, not needed anymore

--- category.py
+++ category.py
@@ -1,16 +1,1 @@
-class CategoryInterface:
-    def __init__(self, master):
-        self.master = master
-        #Initialize category GUI components
-
-    def add_category(self):
-        pass
-
-    def delete_category(self):
-        pass
-
-    def update_category(self):
-        pass
-
-    def list_categories(self):
-        pass
+#We don't need this file anymore, but I couldn't delete it? -Nick
